2e157e48c99daf5310f662e12c100e7930236b4c
Seannnnnnnnnnn <sean.con115@gmail.com>
2025-04-03 20:01:44 +1100

    updated Black Scholes pricing and greek calculations to use dividend yield

--- src/pricing-engines/black_scholes.cpp
+++ src/pricing-engines/black_scholes.cpp
@@ -2,39 +2,33 @@
 #include "../option_base.h"
 #include <cmath>
 
-// TODO: update formulae to use dividend yield adjustment.
-
 // Black-Scholes price calculation
-double BlackScholesPricingEngine::price(const Option& option, double S, double t=0.0, double q=0.0) const {
+double BlackScholesPricingEngine::price(const Option& option, double S, double t, double q) const {
     double K = option.getStrike();
     double T = option.getT();
     double r = option.getR(); 
     double sigma = getVolatility(S, t);
     OptionType type = option.getType();
 
-    double d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
+    double d1 = (log(S / K) + (r - q + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
     double d2 = d1 - sigma * sqrt(T - t);
-    double call_price = S * stdNormalCDF(d1) - K * exp(-r * (T - t)) * stdNormalCDF(d2);
+    double call_price = S * exp(-q * (T - t)) * stdNormalCDF(d1) - K * exp(-r * (T - t)) * stdNormalCDF(d2);
 
-
-    return (type == OptionType::Call) ? call_price : call_price + K * exp(-r * T) - S;
+    return (type == OptionType::Call) ? call_price : call_price + K * exp(-r * (T - t)) - S * exp(-q * (T - t));
 }
-
 
 // Black-Scholes Delta
 double BlackScholesPricingEngine::delta(const Option& option, double S, double t, double q) const {
-    
     double K = option.getStrike();
     double T = option.getT();
     double r = option.getR(); 
     double sigma = getVolatility(S, t);
     OptionType type = option.getType();
 
-    double d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
+    double d1 = (log(S / K) + (r - q + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
 
-    return  (type == OptionType::Call) ? stdNormalCDF(d1) : stdNormalCDF(d1) - 1;     
+    return (type == OptionType::Call) ? exp(-q * (T - t)) * stdNormalCDF(d1) : exp(-q * (T - t)) * (stdNormalCDF(d1) - 1);
 }
-
 
 // Black-Scholes Gamma
 double BlackScholesPricingEngine::gamma(const Option& option, double S, double t, double q) const {
@@ -43,11 +37,10 @@ double BlackScholesPricingEngine::gamma(const Option& option, double S, double t
     double r = option.getR(); 
     double sigma = getVolatility(S, t);
 
-    double d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
+    double d1 = (log(S / K) + (r - q + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
 
-    return stdNormalPDF(d1) / (S * sigma * sqrt(T - t));
+    return (stdNormalPDF(d1) * exp(-q * (T - t))) / (S * sigma * sqrt(T - t));
 }
-
 
 // Black-Scholes Theta
 double BlackScholesPricingEngine::theta(const Option& option, double S, double t, double q) const {
@@ -57,14 +50,15 @@ double BlackScholesPricingEngine::theta(const Option& option, double S, double t
     double sigma = getVolatility(S, t);
     OptionType type = option.getType();
 
-    double d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
+    double d1 = (log(S / K) + (r - q + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
     double d2 = d1 - sigma * sqrt(T - t);
 
-    double firstTerm = -(S * stdNormalPDF(d1) * sigma) / (2 * sqrt(T - t));
+    double firstTerm = -(S * exp(-q * (T - t)) * stdNormalPDF(d1) * sigma) / (2 * sqrt(T - t));
     double secondTerm = r * K * exp(-r * (T - t)) * stdNormalCDF(type == OptionType::Call ? d2 : -d2);
-    return firstTerm - (type == OptionType::Call ? secondTerm : -secondTerm);
+    double thirdTerm = q * S * exp(-q * (T - t)) * stdNormalCDF(type == OptionType::Call ? d1 : -d1);
+    
+    return firstTerm - (type == OptionType::Call ? secondTerm - thirdTerm : -secondTerm + thirdTerm);
 }
-
 
 // Black-Scholes Vega
 double BlackScholesPricingEngine::vega(const Option& option, double S, double t, double q) const {
@@ -73,11 +67,10 @@ double BlackScholesPricingEngine::vega(const Option& option, double S, double t,
     double r = option.getR(); 
     double sigma = getVolatility(S, t);
 
-    double d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
+    double d1 = (log(S / K) + (r - q + 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
 
-    return S * stdNormalPDF(d1) * sqrt(T - t);
+    return S * exp(-q * (T - t)) * stdNormalPDF(d1) * sqrt(T - t);
 }
-
 
 // Black-Scholes Rho
 double BlackScholesPricingEngine::rho(const Option& option, double S, double t, double q) const {
@@ -87,9 +80,7 @@ double BlackScholesPricingEngine::rho(const Option& option, double S, double t, 
     double sigma = getVolatility(S, t);
     OptionType type = option.getType();
 
-    double d2 = (log(S / K) + (r - 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
+    double d2 = (log(S / K) + (r - q - 0.5 * sigma * sigma) * (T - t)) / (sigma * sqrt(T - t));
     
     return (type == OptionType::Call) ? K * (T - t) * exp(-r * (T - t)) * stdNormalCDF(d2) : -K * (T - t) * exp(-r * (T - t)) * stdNormalCDF(-d2);
 }
-
-
